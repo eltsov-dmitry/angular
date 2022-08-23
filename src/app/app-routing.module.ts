07d427b8bc487151fe96e069d877ec898db314d8
Dmitry Eltsov <eltsob.dmitry@gmail.com>
2022-08-23 21:13:56 +0500

    Init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from "./pages/user/login/login.component";
+import {RegisterComponent} from "./pages/user/register/register.component";
+import {RecoveryComponent} from "./pages/user/recovery/recovery.component";
+import {NotFoundComponent} from "./pages/not-found/not-found.component";
+import {MainComponent} from "./pages/main/main.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'recovery', component: RecoveryComponent},
+  {path: '**', component: NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/user/login/login.component';
+import { RegisterComponent } from './pages/user/register/register.component';
+import { RecoveryComponent } from './pages/user/recovery/recovery.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { MainComponent } from './pages/main/main.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    RecoveryComponent,
+    NotFoundComponent,
+    MainComponent,
+    HeaderComponent,
+    FooterComponent,
+    ButtonComponent,
   ],
   imports: [
     BrowserModule,
